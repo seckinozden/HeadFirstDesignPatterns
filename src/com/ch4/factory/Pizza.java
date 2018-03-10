248986c85987f85ea502c0e9aa6db2540d806c72
seckinozden <ozden.seckin@gmail.com>
2018-03-10 10:40:33 +0300

    PizzaStore initialized

---
+++ src/com/ch4/factory/Pizza.java
@@ -0,0 +1,15 @@
+package com.ch4.factory;
+
+public class Pizza {
+    public void prepare() {
+    }
+
+    public void bake() {
+    }
+
+    public void cut() {
+    }
+
+    public void box() {
+    }
+}
---
+++ src/com/ch4/factory/PizzaStore.java
@@ -0,0 +1,20 @@
+package com.ch4.factory;
+
+public abstract class PizzaStore {
+
+    public Pizza orderPizza(String type) {
+
+        Pizza pizza;
+
+        pizza = createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+    public abstract Pizza createPizza(String type);
+}
