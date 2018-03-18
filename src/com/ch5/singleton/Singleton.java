b02722b2f17c7433bd961a2cf90d8b8584a56f77
seckinozden <ozden.seckin@gmail.com>
2018-03-18 23:17:53 +0300

    ch5 singleton initialized

---
+++ src/com/ch5/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package com.ch5.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null)
+            instance = new Singleton();
+
+        return instance;
+    }
+}
