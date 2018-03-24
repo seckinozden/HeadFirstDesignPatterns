2cfcfd0ac8d0bb22094ffbff738677e02cd46f42
seckinozden <ozden.seckin@gmail.com>
2018-03-24 23:06:45 +0300

    garage door ops

---
+++ src/com/ch6/command/GarageDoor.java
@@ -0,0 +1,24 @@
+package com.ch6.command;
+
+public class GarageDoor {
+
+    public void up() {
+        System.out.println("Garage door is up.");
+    }
+
+    public void down() {
+        System.out.println("Garage door is down.");
+    }
+
+    public void stop() {
+        System.out.println("Garage door stopped.");
+    }
+
+    public void lightOn() {
+        System.out.println("Garage door lights turned on.");
+    }
+
+    public void lightOff() {
+        System.out.println("Garage door lights turned off.");
+    }
+}
