b43ea546d3e3748569c37246d9ccf9a44e9810cf
seckinozden <ozden.seckin@gmail.com>
2018-03-30 22:29:41 +0300

    primitive implementations

---
+++ src/com/ch9/iterator/DinerMenu.java
@@ -0,0 +1,34 @@
+package com.ch9.iterator;
+
+public class DinerMenu {
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+
+    public DinerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("Vegetarian BLT",
+                "(Fakin’) Bacon with lettuce & tomato on whole wheat", true, 2.99);
+        addItem("BLT",
+                "Bacon with lettuce & tomato on whole wheat", false, 2.99);
+        addItem("Soup of the day",
+                "Soup of the day, with a side of potato salad", false, 3.29);
+        addItem("Hotdog", "A hot dog, with saurkraut, relish, onions, topped with cheese",
+                false, 3.05);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if (numberOfItems > MAX_ITEMS) {
+            System.out.println("Sorry, Menu is full");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems++;
+        }
+    }
+
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
+}
