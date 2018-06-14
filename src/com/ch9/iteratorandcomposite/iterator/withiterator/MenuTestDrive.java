1fe392651cf617a8b78fc0f9fa368462f872ee3c
seckinozden <ozden.seckin@gmail.com>
2018-06-14 10:55:33 +0300

    waitress with iterator implemented

---
+++ src/com/ch9/iteratorandcomposite/iterator/withiterator/MenuTestDrive.java
@@ -0,0 +1,10 @@
+package com.ch9.iteratorandcomposite.iterator.withiterator;
+
+public class MenuTestDrive {
+    public static void main(String[] args) {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+        waitress.printMenu();
+    }
+}
